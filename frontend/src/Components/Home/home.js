733d0dbd37826bb72c172f9ac02ad1e178cf54c4
Nihindu2002 <manethnihindu@gmail.com>
2025-09-26 14:49:51 +0530

    home css fixed

--- frontend/src/Components/Home/home.js
+++ frontend/src/Components/Home/home.js
@@ -12,7 +12,7 @@ import "./home.css";
 
 const Home = () => {
   return (
-    <div>
+    <div className="home-page">
       <Navbar />
       <Hero />
       <PetStore />
@@ -20,7 +20,6 @@ const Home = () => {
       <Awareness />
       <About />
       <Footer />
-
       <ChatbotButton />
     </div>
   );
